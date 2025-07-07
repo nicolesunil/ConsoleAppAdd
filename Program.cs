bb89c9321a1ab46c2bddf78bb2bbbc5a3f28c96e
nicolesunil <nicolesunil2001@gmail.com>
2025-07-07 16:25:50 +0530

    Addition class and program updated

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAddition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Everyone!");
+            Console.WriteLine("Welcome to Addition");
+
+            AddOp addOp = new AddOp();
+            addOp.Add(10, 20);
+        }
+    }
+}
